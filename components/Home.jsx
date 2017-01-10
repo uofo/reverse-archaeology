a30d677c73b911cff95728e9c4efcd86375adcff
Eric Brelsford <ebrelsford@gmail.com>
2017-01-10 12:35:43 -0500

    Home menu

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import PinnedOverlay from './PinnedOverlay';
+
 import '../styles/components/home.scss';
 
 function Home() {
@@ -8,11 +10,13 @@ function Home() {
     <div>
       <div className="home">
         <div className="home-screen">
-          <ul>
-            <li><Link to="/archive">Enter the archive</Link></li>
-            <li><Link to="/chasm">The chasm is real</Link></li>
-            <li><Link to="/prism">The prism is magic</Link></li>
-          </ul>
+          <PinnedOverlay overlaid={document.body} left={600} top={570}>
+            <ul className="home-menu">
+              <li><Link to="/archive">Enter the archive</Link></li>
+              <li><Link to="/chasm">The chasm is real</Link></li>
+              <li><Link to="/prism">The prism is magic</Link></li>
+            </ul>
+          </PinnedOverlay>
         </div>
       </div>
     </div>
