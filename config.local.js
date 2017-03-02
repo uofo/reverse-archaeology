164ef28183fe9662c73b1ca6907c8996f9547320
Eric Brelsford <ebrelsford@gmail.com>
2017-03-02 11:46:28 -0500

    localhost

--- config.local.js
+++ config.local.js
@@ -1,7 +1,7 @@
 export default {
   dataUrls: {
-    data: 'http://192.168.1.15:4000/reverse-archaeology-content/data/data.json'
+    data: 'http://localhost:4000/reverse-archaeology-content/data/data.json'
   },
 
-  imageUrlBase: 'http://192.168.1.15:4000/reverse-archaeology-content/'
+  imageUrlBase: 'http://localhost:4000/reverse-archaeology-content/'
 };
