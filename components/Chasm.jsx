f203130c9e360d16ace1d5fb6b7145901fb50460
Eric Brelsford <ebrelsford@gmail.com>
2017-04-26 11:31:57 -0400

    Remove mdash
    Closes #63

--- components/Chasm.jsx
+++ components/Chasm.jsx
@@ -178,7 +178,7 @@ class Chasm extends React.Component {
                 </div>
                 <div className="chasm-body-key-text">
 “In that searing moment, I realized the loss he was describing was, in a crucial way, the collective loss. It was the loss of a massive web of connections - a way of being - that had been destroyed by urban renewal; it was as if thousands of people, who seemed to be with me in sunlight, were at some deeper level of their being wandering lost in a dense fog, unable to find one another for the rest of their lives.”
-                  <div className='chasm-body-key-text-attribution'>&mdash;Dr. Mindy Thompson Fullilove, Root Shock: How Tearing Up City Neighborhoods Hurt America, and What We Can Do About It</div>
+                  <div className='chasm-body-key-text-attribution'>--Dr. Mindy Thompson Fullilove, Root Shock: How Tearing Up City Neighborhoods Hurt America, and What We Can Do About It</div>
                 </div>
                 <div style={{ clear: 'both' }}></div>
                 <div className="chasm-body-key-separator"></div>
